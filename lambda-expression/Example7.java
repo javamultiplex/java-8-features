553297bbc5a51bf1ef926e83132af269661d6616
Rohit Agarwal <programmingwithrohit@gmail.com>
2018-01-03 20:43:10 +0530

    Create Example7.java

---
+++ lambda-expression/Example7.java
@@ -0,0 +1,35 @@
+package com.javamultiplex.lambda;
+
+interface abc {
+	public void m1();
+}
+
+/*
+ * Inside lambda expression we can’t declare instance variables. Whatever the
+ * variables declare inside lambda expression are simply acts as local variables
+ * Within lambda expression ‘this” keyword represents current outer class object
+ * reference (that is current enclosing class reference in which we declare
+ * lambda expression)
+ * 
+ */
+public class Example7 {
+
+	int x = 10;
+
+	public void m2() {
+		abc i = () -> {
+			int x = 20;
+			System.out.println(x); // 20
+			System.out.println(this.x); // 10
+		};
+		i.m1();
+	}
+
+	public static void main(String[] args) {
+
+		Example7 ex = new Example7();
+		ex.m2();
+
+	}
+
+}
